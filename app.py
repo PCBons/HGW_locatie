84aaccda47549087ac19cdbc5268d8767715df7a
PCBons <p.c.bons@hva.nl>
2026-03-27 11:51:26 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from streamlit_folium import st_folium
+import folium
+import shapely
+import geopandas as gpd
+
+st.set_page_config(layout="wide")
+
+@st.cache_data
+def import_data(filename):
+    gdf = gpd.read_file(filename)
+    polygon = gdf.iloc[0]['geometry']
+    return polygon
+
+st.title('Zoektocht naar de HGW-locatie')
+st.caption('verken hieronder het gebied wat in 2u bereikbaar is vanaf Utecht met de auto (uitgaande van een vertrektijd 16:00 op 25-09-2026).')
+
+@st.cache_data
+def make_map(_polygon):
+
+    m = folium.Map(location=[_polygon.centroid.y, _polygon.centroid.x], zoom_start=8)
+    folium.GeoJson(shapely.to_geojson(_polygon)).add_to(m)
+    return m
+
+polygon = import_data('polygon_HGW.geojson')
+m = make_map(polygon)
+st_folium(m, width=700, height=500, returned_objects=[])
