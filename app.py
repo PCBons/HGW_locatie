f60c85c4a751eb319431d4f0aa21d0e721fb3312
PCBons <p.c.bons@hva.nl>
2026-03-27 12:18:51 +0100

    add timer

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import streamlit.components.v1 as components
 import folium
 import shapely
+import time
+from datetime import datetime
 import geopandas as gpd
 
 st.set_page_config(layout="wide")
@@ -13,6 +15,25 @@ def import_data(filename):
     return polygon
 
 st.title('Zoektocht naar de HGW-locatie')
+
+@st.fragment(run_every=1)
+def countdown():
+    target = datetime(2026, 4, 3, 18, 0, 0)
+    remaining = target - datetime.now()
+
+    if remaining.total_seconds() <= 0:
+        st.success("🎉 De hint moeten nu bekendgemaakt worden!!")
+        return
+
+    days = remaining.days
+    hours, rem = divmod(remaining.seconds, 3600)
+    minutes, seconds = divmod(rem, 60)
+
+    st.metric("Aftellen tot de volgende hints bekendgemaakt worden...",
+              f"{days}d {hours:02}u {minutes:02}m {seconds:02}s")
+
+countdown()
+
 st.caption('Verken hieronder het gebied wat in 2u bereikbaar is vanaf Utrecht met de auto (uitgaande van een vertrektijd 16:00 op 25-09-2026).')
 
 @st.cache_data
